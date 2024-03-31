5500e1f839ae4acd4974aa5b3eddb411d6c3000d
nayankandalkar <147391041+nayankandalkar@users.noreply.github.com>
2024-03-31 13:14:09 +0530

    Update App.js
    this my commit

--- src/App.js
+++ src/App.js
@@ -14,10 +14,7 @@ function App() {
   return (
     
     <div className="App">
-        {/* <Routes>
-        <Route path='/' element={  <Home></Home> } />
-      </Routes>
-     */}
+       
      <Home></Home>
   
     </div>
